aec950b7b3dff6050e175affdd928fe2acbd4eef
kkupschi <kkupschi@gmail.com>
2026-03-17 17:03:46 +0100

    Add mobile controls with touch events and styling

--- el-pollo-loco/src/app/components/mobile-controls/mobile-controls.ts
+++ el-pollo-loco/src/app/components/mobile-controls/mobile-controls.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { InputService } from '../../core/services/input';
 
 @Component({
   selector: 'app-mobile-controls',
   standalone: true,
-  template: ``
+  templateUrl: './mobile-controls.html',
+  styleUrl: './mobile-controls.css'
 })
-export class MobileControlsComponent { }
+export class MobileControlsComponent {
+
+  constructor(private inputService: InputService) { }
+
+  onLeftStart() { this.inputService.setMoveLeft(true); }
+  onLeftEnd() { this.inputService.setMoveLeft(false); }
+
+  onRightStart() { this.inputService.setMoveRight(true); }
+  onRightEnd() { this.inputService.setMoveRight(false); }
+
+  onJumpStart() { this.inputService.setJump(true); }
+  onJumpEnd() { this.inputService.setJump(false); }
+
+  onThrow() { this.inputService.setThrowBottle(true); }
+}
