2c12ddac67e9cf2a129b8c4df34ad519a2b6f3b1
kkupschi <kkupschi@gmail.com>
2026-03-17 14:39:47 +0100

    add status bar for health, coins and bottles

--- el-pollo-loco/src/app/components/status-bar/status-bar.ts
+++ el-pollo-loco/src/app/components/status-bar/status-bar.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { GameService } from '../../core/services/game';
 
 @Component({
   selector: 'app-status-bar',
   standalone: true,
-  template: ``
+  templateUrl: './status-bar.html',
+  styleUrl: './status-bar.css'
 })
-export class StatusBarComponent { }
+export class StatusBarComponent {
+
+  constructor(public gameService: GameService) { }
+
+  getHealthImage(): string {
+    return this.getStatusImage(this.gameService.characterHealth());
+  }
+
+  getCoinImage(): string {
+    return this.getStatusImage(this.gameService.coins() * 10);
+  }
+
+  getBottleImage(): string {
+    return this.getStatusImage(this.gameService.bottles() * 10);
+  }
+
+  private getStatusImage(value: number): string {
+    if (value >= 100) return '100';
+    if (value >= 80) return '80';
+    if (value >= 60) return '60';
+    if (value >= 40) return '40';
+    if (value >= 20) return '20';
+    return '0';
+  }
+}
