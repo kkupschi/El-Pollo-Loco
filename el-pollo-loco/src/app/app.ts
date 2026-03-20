2f06aadb763e91a169eef8a43df6321374bfa100
kkupschi <kkupschi@gmail.com>
2026-03-20 12:47:46 +0100

    remove angular default template

--- el-pollo-loco/src/app/app.ts
+++ el-pollo-loco/src/app/app.ts
@@ -5,18 +5,6 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `
-    <router-outlet />
-    <div class="rotate-message">
-      <p>🔄 Please rotate your device</p>
-    </div>
-  `,
-  styles: [`
-    :host {
-      display: block;
-      width: 100%;
-      height: 100%;
-    }
-  `]
+  templateUrl: './app.html',
 })
 export class App { }
